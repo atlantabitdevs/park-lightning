7c64e1f3ea4fea8ee2befe1449e6d0c2dfcd6cdb
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-26 17:08:18 -0400

    Update landing page

--- front-end/src/components/DurationSelection.jsx
+++ front-end/src/components/DurationSelection.jsx
@@ -2,13 +2,15 @@ const DurationSelection = () => {
     return (
         <div>
             <h1 className="text-2xl">How long do you want to park?</h1>
-            <div className="flex">
-                <div>Expires 9:30pm</div>
-                <div>+</div>
-            </div>
-            <div className="flex">
-                <div>Price $5 11,250 sats</div>
-                <div>-</div>
+            <div className="bg-cyan-100 rounded-3xl">
+                <div className="flex justify-around">
+                    <div>Expires 9:30pm</div>
+                    <div>+</div>
+                </div>
+                <div className="flex justify-around">
+                    <div>Price $5 11,250 sats</div>
+                    <div>-</div>
+                </div>
             </div>
         </div>
     );
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            minHeight: {
+                '1/2': '50%'
+            }
+        }
     },
     plugins: []
 };
