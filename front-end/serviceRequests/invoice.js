260c1487f6bcfef614798f72d88caf0b830079c1
Alex Lewin <alexlwn123@gmail.com>
2022-03-26 14:33:30 -0400

    added invoice.js

---
+++ front-end/serviceRequests/invoice.js
@@ -0,0 +1,14 @@
+const CreateInvoice = () => {
+  //do Create invoice
+  return {
+    invoiceId: "d8d05dce-dbae-421b-9bdd-ea3ce75b7a77",
+    address: "691 John Wesley Dobbs Ave, Atlanta, GA 30312",
+    occupied: true,
+    licensePlate: "SC39133",
+    start: 1648317620,
+    expiration: 1648319420,
+    duration: 1800
+  }
+
+
+}
