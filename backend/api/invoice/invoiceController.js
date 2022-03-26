da26e35810c9e0ef3eb5bbd32546ac0aabde7395
bnonni <me@bryanwnonni.com>
2022-03-26 14:35:19 -0400

    backend code

--- backend/api/invoice/invoiceController.js
+++ backend/api/invoice/invoiceController.js
@@ -1,11 +1,10 @@
-const broadcastService = require('./invoiceService');
+const invoiceService = require('./invoiceService');
 const debug = require('../../utils/debug');
 
 const createInvoice = async (req, res) => {
   try {
-    const stringData = req.body.stringData
-    const mutationType = req.body.mutationType.toLowerCase();
-    const response = await broadcastService.broadcastString(stringData, mutationType);
+    // TODO add body params
+    const response = await invoiceService.broadcastString(stringData, mutationType);
     
     debug.info(`Create Invoice Response: ${JSON.stringify(response)}`);
     
--- backend/api/spot/spotController.js
+++ backend/api/spot/spotController.js
@@ -1,39 +1,23 @@
-const broadcastService = require('./spotService');
+const spotService = require('./spotService');
 const debug = require('../../utils/debug');
 
-const broadcaster = async (req, res) => {
-  try {
-    const stringData = req.body.stringData
-    const mutationType = req.body.mutationType.toLowerCase();
-    const response = await broadcastService.broadcastString(stringData, mutationType);
-    debug.info(`Broadcaster Response: ${JSON.stringify(response)}`);
-    if (!response.success) {
-      res.status(500).json(response);
-    } else {
-      res.status(200).json(response);
-    }
-  } catch (error) {
-    debug.error(error.stack);
-    res.status(500).json({message: error.message, error: error.stack});
-  }
+const getParkingSpotDetails = async (req, res) => {
+  // try {
+    var query = req._parsedUrl.query.split('=')[1];
+    console.log(query)
+    // const response = await spotService.getParkingSpotDetails();
+    
+    // debug.info(`Spot Response: ${JSON.stringify(response)}`);
+    
+  //   if (!response.success) {
+  //     res.status(500).json(response);
+  //   } else {
+  //     res.status(200).json(response);
+  //   }
+  // } catch (error) {
+  //   debug.error(error.stack);
+  //   res.status(500).json({message: error.message, error: error.stack});
+  // }
 };
 
-const mutations = async (req, res) => {
-  try {
-    const response = await broadcastService.mutations();
-    debug.info(`Mutations Response: ${JSON.stringify(response)}`);
-    if (!response.success) {
-      res.status(500).json(response);
-    } else {
-      res.status(200).json(response);
-    }
-  } catch (error) {
-    debug.error(error.stack);
-    res.status(500).json({message: error.message, error: error.stack});
-  }
-};
-
-module.exports = {
-  broadcaster,
-  mutations
-};
+module.exports = { getParkingSpotDetails };
