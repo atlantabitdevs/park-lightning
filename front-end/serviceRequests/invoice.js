59642d84e15360b2dce477fc8ac4f4c619d2925a
Alex Lewin <alexlwn123@gmail.com>
2022-03-26 15:08:28 -0400

    added dummy requests

--- front-end/serviceRequests/invoice.js
+++ front-end/serviceRequests/invoice.js
@@ -1,13 +1,20 @@
-const CreateInvoice = () => {
+const CreateInvoice = async (satsAmount) => {
+
+  const res = await fetch('https://api/invoice/create', {
+    method: 'POST',
+    body: {
+      satsAmount
+    }
+
+  });
+
+  console.log(res)
   //do Create invoice
+
   return {
-    invoiceId: "d8d05dce-dbae-421b-9bdd-ea3ce75b7a77",
-    address: "691 John Wesley Dobbs Ave, Atlanta, GA 30312",
-    occupied: true,
-    licensePlate: "SC39133",
-    start: 1648317620,
-    expiration: 1648319420,
-    duration: 1800
+    //invoice
+    lnHash: 'lightning:aasdfsdf',
+    expires: 1000
   }
 }
 
