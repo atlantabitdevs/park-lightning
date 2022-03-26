a2640cb483b0a0c86a83a7006d777eacd8b2c87d
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-26 15:04:18 -0400

    Install icons

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { BeakerIcon } from '@heroicons/react/solid';
+
+import { BitcoinIcon } from '@bitcoin-design/bitcoin-icons-react/filled';
+
+function BeakerIconComponent() {
+    return (
+        <div>
+            <BeakerIcon className="h-5 w-5 text-blue-500" />
+        </div>
+    );
+}
+function BitcoinIconComponent() {
+    return (
+        <div>
+            <BitcoinIcon
+                style={{ height: '5px', width: '5px', color: '#F7931A' }}
+            />
+        </div>
+    );
+}
 
 function App() {
     return (
-        <div className="flex justify-center">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-        </div>
+        <>
+            <div className="flex justify-center">
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+            </div>
+            <BeakerIconComponent />
+            <BitcoinIconComponent />
+        </>
     );
 }
 
