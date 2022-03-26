90f0c8decb4eedbbca64431240cd57d1d4b9f646
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-26 14:52:07 -0400

    Update landing page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div className="flex justify-center">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+        </div>
+    );
 }
 
 export default App;
