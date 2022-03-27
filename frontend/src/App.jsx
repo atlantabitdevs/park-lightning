47bec96e8e226452e10334b70f49ff3d9f2d1ddc
Stephen DeLorme <stephen@d.elor.me>
2022-03-27 14:35:25 -0400

    Added step 3 UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import ParkingUserLanding from './components/ParkingUserLanding';
 import { AdminLanding } from './components';
 import { Routes, Route } from 'react-router-dom';
 import Step1 from './components/Step1';
+import Step2 from './components/Step2';
 
 function App() {
     return (
@@ -9,7 +10,8 @@ function App() {
             <Routes>
                 <Route path="admin" element={<AdminLanding />} />
                 <Route path="/" element={<ParkingUserLanding />} />
-              <Route path="/step1" element={<Step1 />} />
+                <Route path="/step1" element={<Step1 />} />
+                <Route path="/step2" element={<Step2 />} />
             </Routes>
         </>
     );
--- frontend/src/components/Step2.jsx
+++ frontend/src/components/Step2.jsx
@@ -5,14 +5,16 @@ import {ArrowLeftIcon, ArrowRightIcon, MinusIcon, PlusIcon} from '@bitcoin-desig
 import Page from './Page';
 import Button from './Button';
 import {Link} from 'react-router-dom';
+import React from 'react';
 
 const ParkingUserLanding = () => {
+    const [location1, setLocation1] = React.useState('Parking Spot 7')
+    const [location2, setLocation2] = React.useState('123 Euclid Ave')
+    const [expiry, setExpiry] = React.useState('9:30pm')
+  
     return (
       <Page>
-        <p className="text-3xl">
-            <strong>Parking Spot 7</strong><br />
-            123 Euclid Ave
-        </p>
+        <OrderDetails location1={location1} location2={location2} expiry={expiry} />
         <p className="text-4xl">
           How long would you like to park?
         </p>
@@ -41,12 +43,12 @@ const ParkingUserLanding = () => {
         </div>
         
         <div className="flex flex-row space-x-4">
-          <Link to="/">
+          <Link to="/step1">
             <Button size="large" importance="secondary" href="/test">
               <span className="sr-only">Back</span> <ArrowLeftIcon className="w-8 h-8" />
             </Button>
           </Link>
-          <Link to="/step2">
+          <Link to="/step3">
             <Button size="large" importance="primary" href="/test">
               <span className="flex flex-row space-x-4"><span>Step 2</span> <ArrowRightIcon className="w-8 h-8" /></span>
             </Button>
